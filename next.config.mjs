52c26cdb6fc21f0e4a681455e1c7baa073834760
SiddDevZ <epic.siddarth@gmail.com>
2025-02-06 17:12:55 +0530

    Trying to fix prod error pt 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-      },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
